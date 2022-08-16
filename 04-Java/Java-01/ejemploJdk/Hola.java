428cc768f138810c786602a1c64113ddd284e3cb
Fero <ferodevelops@gmail.com>
2022-08-16 17:19:47 -0500

    usando intelliJ

---
+++ 04-Java/Java-01/ejemploJdk/Hola.java
@@ -0,0 +1,15 @@
+public class Hola{
+//esto es una clase del JDK Java
+
+	//String miVariable = "Hola mundo java ";
+
+	public static void main (String [] arg){
+	//string miVariable = "Hola mumndo java saludos a: ";
+	String miNombre = "Fero";
+	
+	//es la declaracion del metodo main
+		System.out.println (" Hola! saludos a: " + miNombre);
+	}
+
+
+}
---
+++ 04-Java/Java-01/ejemploJdk/Hola1.java
@@ -0,0 +1,1 @@
+//no se que escribimos aqui x(
